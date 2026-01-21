e9dd332d6115878bc5c273db70cba61cabfde1d5
Jatin Gautam <jatin.gautam@basichomeloan.com>
2026-01-21 23:45:30 +0530

    testing ci/cd pipeline

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ import express from 'express'
 const app = express()
 const PORT = process.env.PORT || 8081
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.json({
         success: true,
-        message: "CI/CD with GitHub Actions is working."
+        message: "CI/CD with GitHub Actions is working. Pipeline test - v1"
     })
 })
 
-app.listen(PORT, ()=>{
+app.get(`/hello/:user`, (req, res) => {
+    const { user } = req.params;
+
+    res.json({
+        success: true,
+        message: `Hello ${user.toUpperCase()}`
+    })
+})
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
