2742434cedc382dc3925d98fe9e8c3766903eb74
Jatin Gautam <jatin.gautam@basichomeloan.com>
2026-01-27 14:05:46 +0530

    add /health endpoint

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
     })
 })
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 app.get(`/:user`, (req, res) => {
     const { user } = req.params;
 
